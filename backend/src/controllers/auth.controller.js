848534797b39adf697158e0df7671294f9cd97cb
uuuuday <krishnauday200301@gmail.com>
2025-07-25 14:05:32 +0530

    add the login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,7 +46,7 @@ const Register = async (req, res) => {
         const token = jwt.sign(
         {
             id:newUser.id,
-        }, process.env.JWT_TOKEN,
+        }, process.env.JWT_SECRET,
         {
             expiresIn:"7d"
         }
@@ -56,9 +56,11 @@ const Register = async (req, res) => {
 
     res.cookie("jwt",token,{        //we are giving res a superpower of cookie //install cookie-parser
     
-         httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV !== "development",
+            httpOnly:true, //only accesible through http js
+            sameSite:"strict", //only accessible when the website opens not when other website redirects to this
+            //helps in CSRF attacks 
+            secure:process.env.NODE_ENV !== "development", //cookie will be only in HTTPS not in HTTP,
+            //but during development u would;nt require https since we wouldnt have SSL key
             maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
     })
         res.status(201).json({
@@ -77,5 +79,73 @@ const Register = async (req, res) => {
             res.status(500).json({
                 error:"Error creating user"
             })
+    }
+}
+
+const login = async(req,res)=>{
+    const {email ,passowrd} = req.body;
+
+
+    if(!email || !passowrd){
+        return res.status(200).josn({
+            message:"email and passowrd is required"
+        })
+    }
+
+    try {
+        const user = await db.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if(!user){
+            return res.status(200).josn({
+                message:"user doesnt exist"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(passowrd,user.passowrd);
+
+        if(!isMatch){
+            return res.status(200).josn({
+                message:"Invalid credentials"
+            })
+        }
+
+        const token =await jwt.sign({
+            id:user.id
+        },process.env.JWT_SECRET,
+        {
+            expiresIn:"7d"
+        }
+
+    )
+
+     res.cookie("jwt" , token , {
+            httpOnly:true,
+            sameSite:"strict",
+            secure:process.env.NODE_ENV !== "development",
+            maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
+        })
+
+        res.status(200).json({
+            success:true,
+            message:"User Logged in successfully",
+            user:{
+                id:user.id,
+                email:user.email,
+                name:user.name,
+                role:user.role,
+                image:user.image //Have to handle in teh future
+            }
+        })
+
+
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({
+            error:"Error logging in user"
+        })
     }
 }
